e54754bf076774b0aa589c11d98651fecb1c6901
igchoi <igchoi@gmail.com>
2022-12-25 21:02:44 +0900

    template html updated

--- mysite/foldsearch/urls.py
+++ mysite/foldsearch/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index') # views -> views.py
-#    path('', views.index, name='index')
+#    path('', views.index, name='index'), # views -> views.py
+    path('', views.upload_file, name='upload_file')  #
 ]
--- mysite/foldsearch/views.py
+++ mysite/foldsearch/views.py
@@ -1,9 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-# Create your views here.
-
+from .forms import UploadFileForm
 from django.http import HttpResponse
 
 def index(request):
     return render(request, 'pdb.html')
 #    return HttpResponse("Hello, World!")
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+          #  return HttpResponseRedirect('/success/url/')
+            return HttpResponse("Successfully uploaded & saved")
+    else:
+        form = UploadFileForm()
+    return render(request, 'pdb.html', {'form': form})
+
+def handle_uploaded_file(f):
+    with open('tmp.pdb', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
