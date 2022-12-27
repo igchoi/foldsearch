a6477bef3da1f6b796e3c015bb94da17e3b6f2ae
In-Geol Choi <igchoi@gmail.com>
2022-12-27 21:43:55 +0900

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from mysite import views
 
 urlpatterns = [
+    path('', views.index, name="main"),
     path('foldsearch/', include('foldsearch.urls')),
     path('admin/', admin.site.urls),
 ]
